d2d382a089ee06d93afec3650b297d63b16fe8bf
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-08 07:50:21 +0700

    first commit

---
+++ src/ardhastudio/classes/StringApp.java
@@ -0,0 +1,11 @@
+package ardhastudio.classes;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Fathar Dhabit Adz-Dzaky";
+
+        String nameLowerCase = name.toLowerCase();
+        System.out.println(nameLowerCase);
+        System.out.println("=======================================");
+    }
+}
