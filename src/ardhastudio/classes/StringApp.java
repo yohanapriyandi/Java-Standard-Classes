4d95f1a107863492be49a54caf214128ed12134d
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-09 09:20:09 +0700

    explore string classes

--- src/ardhastudio/classes/StringApp.java
+++ src/ardhastudio/classes/StringApp.java
@@ -2,10 +2,16 @@ package ardhastudio.classes;
 
 public class StringApp {
     public static void main(String[] args) {
-        String name = "Fathar Dhabit Adz-Dzaky";
+        String name = "Fathar Dhabit Adz-Dzaky ";
 
         String nameLowerCase = name.toLowerCase();
         System.out.println(nameLowerCase);
         System.out.println("=======================================");
+        String nameToUppercase = name.toUpperCase();
+        System.out.println(nameToUppercase);
+        System.out.println("=======================================");
+        System.out.println(name.length());
+        System.out.println(name.trim());
+        System.out.println(name.toCharArray());
     }
 }
