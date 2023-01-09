808d5db15b5995e4fae526d7741b176ea4d6c9e1
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-09 12:00:34 +0700

    Math Classes

---
+++ src/ardhastudio/classes/MathApp.java
@@ -0,0 +1,17 @@
+package ardhastudio.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(10,20);
+        System.out.println(min);
+
+        var max = Math.max(10,20);
+        System.out.println(max);
+
+        var pi  = Math.PI;
+        System.out.println(pi);
+
+        System.out.println(Math.floor(1347.12774877));
+
+    }
+}
