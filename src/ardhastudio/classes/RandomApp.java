4451c6a3e6b9fe4c33363009173eca997801dd2c
Yohan Apriyandi <cepoangkuningan@gmail.com>
2023-01-10 11:18:32 +0700

    RandomClasses

---
+++ src/ardhastudio/classes/RandomApp.java
@@ -0,0 +1,15 @@
+package ardhastudio.classes;
+
+import java.util.Random;
+
+public class RandomApp {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i =0; i<100; i++){
+            int value = random.nextInt(1000);
+            System.out.println(value);
+
+        }
+    }
+}
